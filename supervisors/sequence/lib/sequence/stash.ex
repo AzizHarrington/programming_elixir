244b2bbbe822f92c20de5130ec5f9031fba4a2fd
Aziz Harrington <aharrington@leapfrogonline.com>
2015-09-30 16:16:48 -0500

    hot code loading : )

--- supervisors/sequence/lib/sequence/stash.ex
+++ supervisors/sequence/lib/sequence/stash.ex
@@ -1,15 +1,50 @@
+# defmodule Sequence.Stash do
+#   use GenServer
+
+#   @vsn "0"
+#
+#   #####
+#   # External API
+#
+#   def start_link(current_number) do
+#     {:ok, _pid} = GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+#   end
+#
+#   def save_value(pid, value) do
+#     GenServer.cast pid, {:save_value, value}
+#   end
+#
+#   def get_value(pid) do
+#     GenServer.call pid, :get_value
+#   end
+#
+#   #####
+#   # GenServer implementation
+#
+#   def handle_call(:get_value, _from, current_value) do
+#     { :reply, current_value, current_value }
+#   end
+#
+#   def handle_cast({:save_value, value}, _current_value) do
+#     { :noreply, value }
+#   end
+# end
+
 defmodule Sequence.Stash do
   use GenServer
+  require Logger
+
+  @vsn "1"
 
   #####
   # External API
 
   def start_link(current_number) do
-    {:ok, _pid} = GenServer.start_link(__MODULE__, current_number)
+    {:ok, _pid} = GenServer.start_link(__MODULE__, {current_number, 1}, name: __MODULE__)
   end
 
-  def save_value(pid, value) do
-    GenServer.cast pid, {:save_value, value}
+  def save_value(pid, {value, delta}) do
+    GenServer.cast pid, {:save_value, {value, delta}}
   end
 
   def get_value(pid) do
@@ -19,11 +54,19 @@ defmodule Sequence.Stash do
   #####
   # GenServer implementation
 
-  def handle_call(:get_value, _from, current_value) do
-    { :reply, current_value, current_value }
+  def handle_call(:get_value, _from, {value, delta}) do
+    { :reply, {value, delta}, {value, delta} }
   end
 
-  def handle_cast({:save_value, value}, _current_value) do
-    { :noreply, value }
+  def handle_cast({:save_value, {value, delta}}, {_v, _d}) do
+    { :noreply, {value, delta} }
+  end
+
+  def code_change("0", old_state = current_value, _extra) do
+    new_state = {current_value, 1}
+    Logger.info "changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+    { :ok, new_state }
   end
 end
